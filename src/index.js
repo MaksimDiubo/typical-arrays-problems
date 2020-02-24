79bfdec77bb5a679b8ca71b7861537a1bb7fdfd9
Maksim Diubo <diubomaksim@gmail.com>
2020-02-24 22:25:20 +0300

    feat: solve Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array.length < 1) {
+        return 0;
+    } else {
+        return array.length ? array.sort( (a, b) => a - b )[0] : 0;
+    }
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length < 1) {
+        return 0;
+    } else {
+        return array.length ? array.sort( (a, b) => b - a )[0] : 0;
+    }  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array.length < 1) {
+        return 0;
+    } else {
+        let avg = 0;
+        for (let el of array) {
+            avg += el;
+        }
+      return avg / array.length;
+    }
 }
